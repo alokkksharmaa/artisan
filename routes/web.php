08893476e47c64a5b2c7965df44cd6d0b931f2f4
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-03 19:06:33 +0530

    new files added

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('hello');
+    echo("Hi");
+});
+
+
+// direct method to fetch the view
+Route::view('/welcome', "welcome");
+
+Route::get("/table" , function()  {
+    return view('table');
 });
