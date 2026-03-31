e1e2e6c35ce4c769234410cf6535173f19396768
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-31 19:38:16 +0530

    stagged

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +21,4 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 
-Route::get('/dashboard', [DashboardController::class, 'show']);
+// Route::get('/dashboard', [DashboardController::class, 'show']);
