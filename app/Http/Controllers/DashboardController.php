c688e3fdd5437057628e3e47dcaa8393bb07d348
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-31 11:23:42 +0530

    middleware & controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        // initialization logic only
+        $this->middleware('auth');
+    }
+
+    public function show()
+    {
+        return "Hello World";
+    }
+}
