6381b454db426997230bffe232bfef36ed7f2e3f
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-05 10:20:43 +0530

    basic routing and passing data to the different methods

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,49 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::any('/', function() {
+Route::any('/', function () {
     return "Home";
 });
 
-Route::get('/home', function() {
+Route::get('/home', function () {
     return "Hello from home";
 });
 
+Route::post('/submit-post', function () {
+
+    return "Form Submission";
+});
+
+
+
+// DYNAMIC Routing;
+
+Route::any('/user/{id}', function ($id) {
+    return "User " . $id;
+});
+
+
+// MULTIPLE Routing
+
+
+Route::any('user/{id}/{name}', function ($id, $name) {
+    return "User id is: " . $id  . "user name is: " . $name;
+});
+
+
+// optional paramater
+Route::get('/user/{name?}', function ($name = "Guest") {
+
+    return $name;
+});
+
+
+// PASSING DATA to views
+
+// by array
+Route::get('/home', function(){
+
+    return view('home', [
+        'name' => "Alok"
+    ]);
+});
