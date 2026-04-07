e226cc444d6e30d6219bcddbef7c1e860bae33ba
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-07 13:45:16 +0530

    igt ca1

---
+++ resources/views/clothes-list.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Clothes Shop</title>
+</head>
+<body>
+
+<h2>Clothes available at the Shop</h2>
+
+<ul>
+
+<li>
+<a href="{{ url('/tshirt') }}">
+Tshirt - ₹500
+</a>
+</li>
+
+<li>
+<a href="{{ url('/jeans') }}">
+Jeans - ₹1200
+</a>
+</li>
+
+<li>
+<a href="{{ url('/jacket') }}">
+Jacket - ₹3000
+</a>
+</li>
+
+<li>
+<a href="{{ url('/sweater') }}">
+Sweater - ₹800
+</a>
+</li>
+
+<li>
+<a href="{{ url('/dress') }}">
+Dress - ₹1500
+</a>
+</li>
+
+</ul>
+
+</body>
+</html>
---
+++ resources/views/jacket.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jacket</title>
+</head>
+<body>
+
+<h2>Jacket</h2>
+
+Price ₹3000
+
+<br><br>
+
+Description : Leather jacket with zip code closure
+
+<br><br>
+
+<a href="{{ url('/clothes') }}">
+Back to Clothes List
+</a>
+
+</body>
+</html>
