a9ec1fb59fc43984cfdf5f4a56dcfdd981ce323f
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-20 23:14:32 +0530

    Unit 5

--- app/Rules/CheckAge.php
+++ app/Rules/CheckAge.php
@@ -16,23 +16,6 @@ class CheckAge implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         //
-        // app/Rules/CheckAge.php
-
-namespace App\Rules;
-
-use Illuminate\Contracts\Validation\Rule;
-
-class CheckAge implements Rule
-{
-    public function passes($attribute, $value)
-    {
-        return $value >= 18;
-    }
-
-    public function message()
-    {
-        return "Age must be 18 or above";
-    }
-}
+        
     }
 }
