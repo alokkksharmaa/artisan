82776f32afadd9098f86f60d1bd61215f4c62dfb
alokkksharmaa <aloksharma1097@gmail.com>
2026-06-04 23:35:38 +0530

    mongodb database configred

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Student;
-use Illuminate\Support\Facades\File;
 
 class StudentSeeder extends Seeder
 {
@@ -13,23 +12,6 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        // Insert single record
-        Student::create([
-            'name'  => 'Raj',
-            'email' => 'abc@gmail.com',
-            'age'   => 28,
-        ]);
-
-        $json = File::get('database/json/student.json');
-        $students = collect(json_decode($json));
-
-        $students->each(function ($student) {
-            Student::create([
-                'name' => $student->name,
-                'email' => $student->email,
-                'age' => $student->age
-            ]);
-        });
-
+        //
     }
 }
